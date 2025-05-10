8411502c8fcf7f32814fb98160b4c94a222da70e
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-05-10 11:21:22 -0700

    basic

--- second.js
+++ second.js
@@ -1,3 +1,33 @@
-console.log("hello this is external file")
-document.write("hello sagar")
-alert("warning")
+
+// alert("warning")
+// var a=9 //redeclared variable a
+
+// let a=5
+// a=14
+// console.log(a+1)
+
+// let b=4
+// let c=a+b
+// console.log(a+b)
+// let r=5
+// document.write(3.14*r*r)
+// var d=10
+// var d=12
+// console.log(d+d)
+
+// 10/5/25
+// var a= parseInt(prompt("enter a"))
+// var b= parseInt(prompt("enter b"))
+
+// console.log(a+b)
+
+let r=parseFloat(prompt("enter radius of circle"))
+console.log("area of circle ="+3.14*r*r)
+
+let b=parseInt(prompt("enter a number"))
+console.log(b*b*b)
+
+let c=parseInt(prompt("enter first num"))
+let d=parseInt(prompt("enter second num"))
+let e=c+d
+console.log(e*e)
