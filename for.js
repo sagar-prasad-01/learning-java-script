b0972461e3d58a2934512aeab6b5e27558a8bb53
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-05-15 08:59:27 -0700

    for-loop

---
+++ for.js
@@ -0,0 +1,46 @@
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(i);
+    
+// }
+
+// for(let i=20;i>=10;i--)
+// {
+// console.log(i);
+
+// }
+
+
+// let n=parseInt(prompt("enter number"))
+//     let k=n*n;
+   
+
+//     fo}r(let i=10;i>=1;i--)
+//     {
+//         console.log(k+"*" + i + "=" + (k*i))
+//     
+    
+
+
+
+// for(let i=1;i<=15;i++)
+// {
+//     if(i%2==0)
+//     {
+//         console.log(i);
+        
+//     }
+// }
+
+let n=parseInt(prompt("enter number"))
+if(n%2==0)
+{
+for(let i=1;i<=10;i++)
+    {
+       console.log(n+"*" + i + "=" + (n*i));
+   
+}
+}
+else{
+    console.log(n*n*n);
+}
