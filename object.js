07f66bfecbcaa330eab65d6c6c9ea365b2569f74
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-05-20 21:09:51 +0530

    objects

---
+++ object.js
@@ -0,0 +1,28 @@
+
+
+// let obj={
+// name:prompt("enter your name"),
+// age:prompt("enter your age"),
+// contact:prompt("enter your contact")
+
+
+// }
+// // obj.name=prompt("enter name ")
+// // obj.age=prompt("enter age ")
+// // obj.contact=prompt("enter contact ")
+
+
+// console.log(obj)
+
+
+let emp = {
+    name:"sagar",
+    age:18,
+    id:1,
+    emp_acc: {
+        acc_no:272157,
+        acc_balance:500
+    }
+
+}
+console.log(emp.emp_acc.acc_balance);
