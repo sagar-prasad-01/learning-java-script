96c042996fdfddaf6b879748d66664eb4a36adc0
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-05-21 21:26:32 +0530

    object

---
+++ object2.js
@@ -0,0 +1,10 @@
+let students={
+    name:"sagar",
+    age:18,
+    State:"bihar"
+
+}
+for(let key in students)
+{
+    console.log(key)
+}
---
+++ test1.js
@@ -0,0 +1,9 @@
+let arr=[1,2,3,4,5,6,7,8,9]
+let pice=arr.slice(5,9)
+console.log(pice)
+let mul=1;
+for(let k of arr)
+{
+     mul=mul*k;
+}
+console.log(mul)
