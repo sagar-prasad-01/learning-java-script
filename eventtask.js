c2939eeb37754f0d1d6e14d6561416a6d2b2b5ac
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-05-31 21:06:20 +0530

    events

---
+++ eventtask.js
@@ -0,0 +1,11 @@
+function fun()
+{
+    let a=parseInt(prompt("enter one num"))
+    let b=parseInt(prompt("enter sec num"))
+    console.log("sum = " + (a+b))
+}
+function fun1(cl)
+{
+    
+    document.body.style.backgroundColor=cl
+}
