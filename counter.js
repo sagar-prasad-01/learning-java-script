09e67f4838839b4011bc162bc9f298a035ce7bac
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-06-03 00:48:59 +0530

    counter

--- counter.js
+++ counter.js
@@ -1,11 +1,22 @@
 let n=0
-
+ h2=document.getElementById('count')
 
 function increment()
 {
     n++
-    document.getElementById('count').innerHTML=n
-}
+    h2.innerHTML=n
+    if(n%2==0)
+    {
+         h2.style.color="red"
+    }
+    else
+    {
+         h2.style.color="black"
+    }
+    }
+
+   
+
 function decrement()
 {
     if(n>0)
@@ -13,24 +24,35 @@ function decrement()
         n--
     }
 
-    document.getElementById('count').innerHTML=n
+   h2.innerHTML=n
+   if(n%2==0 & n!=0)
+    {
+         h2.style.color="red"
+    }
+    else
+    {
+         h2.style.color="black"
+    }
 }
 function reset()
 {
     n=0 
-    document.getElementById('count').innerHTML=n
+    h2.innerHTML=n
+    h2.style.color="black"
 }
+
+
 //even num come in red
 
 
-let h1=document.getElementById('red')
-function func1()
-{
-    h1.style.color="blue"
-    h1.textContent="get out"
-}
-function func2()
-{
-    h1.style.color="black"
-    h1.textContent="Hover on me"
-}
+// let h1=document.getElementById('red')
+// function func1()
+// {
+//     h1.style.color="blue"
+//     h1.textContent="get out"
+// }
+// function func2()
+// {
+//     h1.style.color="black"
+//     h1.textContent="Hover on me"
+// }
