0ad1cdd22dab116185ebf084880431ca892e5591
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-06-06 20:11:32 +0530

    event key

--- eventchaycode.js
+++ eventchaycode.js
@@ -1,1 +1,9 @@
-let x=
+
+function fun1()
+{
+document.querySelector('.container').style.backgroundColor='red';
+}
+function fun2()
+{
+document.querySelector('.container').style.backgroundColor='yellow';
+}
