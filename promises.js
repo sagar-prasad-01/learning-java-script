43ac6c01c5a7f1f00e6b249de330d99f9bff7091
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-06-30 21:29:16 +0530

    api

--- promises.js
+++ promises.js
@@ -32,5 +32,28 @@ p.then((e)=>{
     data.map((e)=>{
         console.log(e.id)
     })
+    let apidata=document.getElementById("dataofapi")
+     apidata.innerHTML=data.map((e)=>{
+        return`
+         <div style="width:300px; height: 400px; background-color:lightpink; border:2px solid black; box-shadow: 10px 10px 10px red; " >
+        <h2>BRAND:${e.brand}<h2>
+        <h2>CATEGORY:${e.category}<h2>
+        <h3>Title:${e.title}</h3>
+        <img src="${e.image}" alt="image" height="100px">
+        <br>
+        <strong>Price:${e.price}</strong> <br>
+        <button onclick="addprice(${e.price})">ADD</button>
+        <button onclick="subprice(${e.price})">SUB</button>
+        </div>
+        `
+    })
+  
+    // window.addprice(){
+    //     alert("hello")
+    // }
 
+    // window.subprice(){
+    //     alert("dcds")
+    // }
 })
+ 
