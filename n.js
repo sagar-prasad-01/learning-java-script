00cba94e80185947e2cf42ecefa8f2349196e888
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-10-08 10:38:36 +0530

    loop

--- n.js
+++ n.js
@@ -1,1 +1,9 @@
-console.log("hello js");
+let a=10;
+let b=5;
+document.write("sum ="+a+b)
+alert(a*a)
+let r=2;
+console.log("area of circle = "+3.14*r*r)
+let l=5;
+let w=10;
+console.log("area of rectangle = "+(l*b));
