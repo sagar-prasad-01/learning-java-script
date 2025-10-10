6de067faa1ff250180799211dabf25071c2dea78
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-10-10 18:45:14 +0530

    if else basic

--- n.js
+++ n.js
@@ -1,9 +1,66 @@
-let a=10;
-let b=5;
-document.write("sum ="+a+b)
-alert(a*a)
-let r=2;
-console.log("area of circle = "+3.14*r*r)
-let l=5;
-let w=10;
-console.log("area of rectangle = "+(l*b));
+// let a=10;
+// let b=5;
+// document.write("sum ="+a+b)
+// alert(a*a)
+// let r=2;
+// console.log("area of circle = "+3.14*r*r)
+// let l=5;
+// let w=10;
+// console.log("area of rectangle = "+(l*b));
+// console.log("ravi kumar hero")
+// document.write("sum  ravi kumar =")
+
+
+
+
+
+//wap to check number is greater than 15 or not
+// let x=parseInt(prompt("enter any number"))
+// if (x>15) {
+//     alert("greater than 15");
+    
+// }
+// else{
+//     alert("no");
+// }
+
+
+//1
+let x=parseInt(prompt("enter any number"))
+if (x%3==0) {
+    alert("divisible by 3");
+    
+}
+else{
+    alert("not divisible by three");
+}
+
+//2
+if (x%2==0) {
+    alert("even number");
+    
+}
+else{
+    alert("odd number");
+}
+
+//3
+let n1=parseInt(prompt("enter number 1"));
+let n2=parseInt(prompt("enter number 2"));
+if (n2%2==0) {
+    
+    alert(3.14*n1*n1);
+}
+else{
+    alert(n1*n1*n1);
+}
+
+//4
+let y=parseInt(prompt("enter any number"))
+if (y>0) {
+    alert(y*y);
+    
+}
+else{
+    alert(y);
+}
