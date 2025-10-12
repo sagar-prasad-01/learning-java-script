a490a358562dcbb304557dbdf05d71880d133ef8
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-10-12 17:52:35 +0530

    if else

--- n1.js
+++ n1.js
@@ -1,25 +1,114 @@
-let p=parseInt(prompt("enter marks of phy"))
-let c=parseInt(prompt("enter marks of chemistry"))
-let m=parseInt(prompt("enter marks of math"))
-let h=parseInt(prompt("enter marks of hindi"))
-let e=parseInt(prompt("enter marks of english"))
-let x=(p+c+m+h+e)/5;
-if(x>89){
-console.log("grade is a+")
-}
-else if(x>79){
-console.log("grade is a")
+// let p=parseInt(prompt("enter marks of phy"))
+// let c=parseInt(prompt("enter marks of chemistry"))
+// let m=parseInt(prompt("enter marks of math"))
+// let h=parseInt(prompt("enter marks of hindi"))
+// let e=parseInt(prompt("enter marks of english"))
+// let x=(p+c+m+h+e)/5;
+// if(x>89){
+// console.log("grade is a+")
+// }
+// else if(x>79){
+// console.log("grade is a")
 
-}
-else if(x>69){
-console.log("grade is b")
+// }
+// else if(x>69){
+// console.log("grade is b")
 
-}
-else if(x>59){
-console.log("grade is c")
+// }
+// else if(x>59){
+// console.log("grade is c")
 
-}
-else{
-    console.log("you fail(F)");
+// }
+// else{
+//     console.log("you fail(F)");
     
+// }
+
+
+// let n=parseInt(prompt("enter number"));
+// if(n%2==0){
+//     //even
+//     if(n>=0 && n<=20){
+
+//         console.log(n*n*n);
+//     }
+//     else{
+//         console.log(n*n);
+//     }
+
+// }
+
+// else{
+//     //odd
+//     console.log(n);
+// }
+
+/*
+let n=parseInt(prompt("enter number"));
+if(n>0){
+    if(n%3==0){
+
+        console.log("num is divisible by 3");
+    }
+    else{
+        console.log("num is not div by 3");
+    }
+
+}
+
+else{
+    console.log(n*n);
+}
+
+let x=parseInt(prompt("enter number"));
+switch(x) {
+    case 1:
+        console.log("1111")
+        break;
+    case 2:
+        console.log("2222")
+        break;
+    case 3:
+        console.log("333")
+        break;
+        
+     default:
+        console.log("invalid input")
+}
+*/
+
+let c=prompt("enter any charcter");
+switch (c) {
+    case 'a':
+        console.log("vowel");
+        break;
+    case 'e':
+        console.log("vowel");
+        break;
+    case 'i':
+        console.log("vowel");
+        break;
+    case 'o':
+        console.log("vowel");
+        break;
+    case 'u':
+        console.log("vowel");
+        break;
+  
+    case 'E':
+        console.log("vowel");
+        break;
+    case 'I':
+        console.log("vowel");
+        break;
+    case 'O':
+        console.log("vowel");
+        break;
+    case 'U':
+        console.log("vowel");
+        break;
+        
+        default:
+        console.log("not vowel");
+        break;
 }
