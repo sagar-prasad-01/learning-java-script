1983bb84c2cc02e922c9a132d2bd996437cf395a
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-10-13 23:18:39 +0530

    loop

---
+++ loopn.js
@@ -0,0 +1,37 @@
+let x=10;
+let t=parseInt(prompt("enter number"));
+do{
+    console.log(t+"x"+x+"="+(t*x));
+x--;
+}
+while(x>=1);
+// let a=25;
+// while(a>=1){
+//     console.log(a);
+//     a--;  
+// }
+
+// let k=80;
+// while(k>=1){
+//     if(k%2!=0)
+//         {
+//         console.log(k*k);
+        
+//     }
+//     k--;  
+// }
+
+
+// let m=5;
+// while(m<=25){
+//     if(m%2==0)
+//         {
+//         console.log(m);
+        
+//     }
+//     m++;  
+// }
+
+// //wap to print only even number btw 5 to 25
+// //wap to print digits btw 25 to 1
+// //wap to print square of all odd number btw 80 to 1
