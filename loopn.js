5a86fbcc9620cfaca32371612f4bfb0bff056eb9
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-10-14 19:16:44 +0530

    for loop

--- loopn.js
+++ loopn.js
@@ -1,37 +1,100 @@
-let x=10;
-let t=parseInt(prompt("enter number"));
-do{
-    console.log(t+"x"+x+"="+(t*x));
-x--;
-}
-while(x>=1);
-// let a=25;
-// while(a>=1){
-//     console.log(a);
-//     a--;  
+// let x=10;
+// let t=parseInt(prompt("enter number"));
+// do{
+//     console.log(t+"x"+x+"="+(t*x));
+// x--;
 // }
+// while(x>=1);
+// // let a=25;
+// // while(a>=1){
+// //     console.log(a);
+// //     a--;  
+// // }
 
-// let k=80;
-// while(k>=1){
-//     if(k%2!=0)
-//         {
-//         console.log(k*k);
+// // let k=80;
+// // while(k>=1){
+// //     if(k%2!=0)
+// //         {
+// //         console.log(k*k);
         
-//     }
-//     k--;  
-// }
+// //     }
+// //     k--;  
+// // }
 
 
-// let m=5;
-// while(m<=25){
-//     if(m%2==0)
-//         {
-//         console.log(m);
+// // let m=5;
+// // while(m<=25){
+// //     if(m%2==0)
+// //         {
+// //         console.log(m);
         
-//     }
-//     m++;  
+// //     }
+// //     m++;  
+// // }
+
+// // //wap to print only even number btw 5 to 25
+// // //wap to print digits btw 25 to 1
+// // //wap to print square of all odd number btw 80 to 1
+
+// let n=2;
+// for (let i = 10; i >=1; i--){
+//   console.log(n*i);
+ 
 // }
 
-// //wap to print only even number btw 5 to 25
-// //wap to print digits btw 25 to 1
-// //wap to print square of all odd number btw 80 to 1
+// let sum=0;
+// for (let i = 10; i>=1; i--){
+//  sum=sum+i;
+// }
+// console.log(sum);
+
+
+// for (let i = 20; i >=1; i--){
+//   if(i%2==0){
+//     console.log(i*i);
+
+//   }
+ 
+// }
+
+
+// let k=parseInt(prompt("enter number"));
+// if(k>0 && k<11){
+//     for (let i = 1;i<=k ; i++) {
+//         console.log(i); 
+//     }
+
+// }
+// else if(k<21){
+//     for (let i = 20;i>=1 ; i--) {
+//         console.log(i); 
+//     }
+// }
+// else{
+//     console.log("invalid input"); 
+// }
+
+
+   let m=parseInt(prompt("enter number 1"));
+   let n=parseInt(prompt("enter number 2"));
+    let mul=1;
+   if(n%3==0){
+    for (let i = 10;i>=1 ; i--) {
+        if(i%2!=0){
+            console.log(i);
+        }   
+    }
+   }
+   else{
+     for (let i = 5;i>=1 ; i--) {
+        mul=mul*i;
+    }
+    console.log(mul+1);
+   }
+
+  
+
+//wap to print a reverse table 
+//wap to print sum of all num btw 1 to 10
+//wap to print square of even number btw 20-1
+//
