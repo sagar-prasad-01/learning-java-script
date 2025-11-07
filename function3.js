b4db38711d8c2d4e6201e8be81546136f33ffa19
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-11-07 18:17:31 +0530

    function

--- function3.js
+++ function3.js
@@ -1,5 +1,35 @@
-function Demo(params) {
-    console.log("inside function");
-    
+function Sum() {
+   let a=5;
+   let b=10;
+   console.log("sum=",a+b);  
 }
-Demo()
+Sum()
+
+
+
+function EvenOdd(n){
+    if(n%2==0){
+        console.log("even numbeer");
+        
+    }
+    else{
+        console.log("odd number");
+        
+    }
+
+}
+console.log(EvenOdd(12));
+
+
+// function Table(){
+//     for(let i=10;i>=1;i--){
+
+//         console.log(i*n);
+        
+//     }
+
+// }
+//  Table()
+//wap to add two number using function
+//wap take input from user and check number is odd or even
+//wap to print reverse table of any number using function
