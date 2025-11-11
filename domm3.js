a7557fe676e17b1ad400e7ee3058e515c6bae49d
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-11-11 18:45:20 +0530

    events

---
+++ domm3.js
@@ -0,0 +1,9 @@
+document.getElementById("img").src="bewkoof_img/2s,1.webp"
+function fun(){
+    alert("hellllllo")
+}
+function fun2(){
+    document.getElementById("change").textContent="change kar diye"
+}
+
+
