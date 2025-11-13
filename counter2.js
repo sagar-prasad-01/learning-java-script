9c5f80d8eee175fa4597b6bda4894ec84213bad8
sagar-prasad-01 <sagarmadheshiya82@gmail.com>
2025-11-13 22:42:45 +0530

    callback fun

--- counter2.js
+++ counter2.js
@@ -11,3 +11,24 @@ function counter3(){
     document.getElementById("cnt").textContent=number;
 }
 
+
+
+function img1(){
+    document.getElementById("img4").src="bewkoof_img/1s,1.webp" 
+}
+function img2(){
+    document.getElementById("img4").src="bewkoof_img/2s,1.webp" 
+}
+function img3(){
+    document.getElementById("img4").src="bewkoof_img/3s,1.webp" 
+}
+
+
+
+document.querySelector(".hii").style.color="red" 
+document.querySelectorAll("h3")[2].style.color="red"
+
+
+btn=document.querySelector(".btn");
+
+btn.addEventListener('click',()=>{alert("hello")});
